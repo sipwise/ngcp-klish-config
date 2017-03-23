a1692865832ecbd254a07af23280d1fab602befe
Alexander Lutay <alutay@sipwise.com>
2017-03-23 13:39:40 +0100

    TT#12759 Fix shellcheck warnings for run_tests.sh
    I'm afraid the tests are broken here (at least I was not
    able to pass them, while the trace for old and new scripts
    are identical and shellcheck is happy here now).
    
    Change-Id: I661a17fa70ba939385a48fa06e95ade5004b6e2f
    (cherry picked from commit e0a64064fdfc3517da698b9de66a3a7c08d21712)

--- run_tests.sh
+++ run_tests.sh
@@ -5,8 +5,11 @@ rm -rf reports/*
 
 function run()
 {
-	echo "testing $1 -> reports/$2.tap"
-			cat<<EOF|lua5.1 - > reports/$2.tap
+  local tmpfile
+  tmpfile=$(mktemp)
+
+  echo "testing $1 -> reports/$2.tap"
+  cat > "${tmpfile}" << EOF
 require "tests/$2"
 ---- Control test output:
 lu = LuaUnit
@@ -14,21 +17,22 @@ lu:setOutputType('TAP')
 lu:setVerbosity(1)
 lu:run()
 EOF
+  lua5.1 - < "${tmpfile}" > "reports/$2.tap"
+  rm -f "${tmpfile}"
 }
 
 if [[ -z "$@" ]]; then
-	TESTS=$(find tests -name '*.lua' -exec basename {} .lua \;|xargs)
+	TESTS="$(find tests -name '*.lua' -exec basename {} .lua \;)"
 else
-	TESTS=$@
+	TESTS=$*
 fi
 
 for i in $TESTS; do
 	f="tests/$i.lua"
-	if [ ! -f $f ]; then
+	if [ ! -f "$f" ]; then
 		echo "No $f found"
 	else
-		run $f $i
+		run "$f" "$i"
 	fi
 done
 exit 0
-#EOF
