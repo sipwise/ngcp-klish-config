bf30d541fc9a7e848e99a665f529f65ee1b302d6
Victor Seva <vseva@sipwise.com>
2020-04-01 17:43:19 +0200

    TT#64357 switch to lua5.2
    * don't use globals, don't use module
      https://www.lua.org/manual/5.2/manual.html#8.2
    * be aware that ngcp-klish/ngcp.lua is NOT a module
    * clean luacheck errors/warnings
    * remove lua-uri dependency
    * migrate tests from lua-unit and lua-lemock to lua-busted
    
    WARNING: ngcp-klish/ngcp.lua is outdated and most not work
    with actual kamailio config
    
    Change-Id: I10c6b674bc68ac07c3f02db072d6bff5ba2267a1
    (cherry picked from commit 595a5caaccef23faf7922d5e417df43b1c592fdb)

--- ngcp-klish/expand.lua
+++ ngcp-klish/expand.lua
@@ -2,55 +2,56 @@
 -- http://lua-users.org/files/wiki_insecure/users/pshook/template/Expand.lua
 local strfind = string.find
 local strsub  = string.sub
+local gmatch  = string.gmatch
 local push    = table.insert
 local pop     = table.remove
 local concat  = table.concat
 
 local statements = {}
-for w in string.gfind('do if for while repeat', '%a+') do
+for w in gmatch('do if for while repeat', '%a+') do
   statements[w] = true
 end
 
-function expand(str, ...)
+return function (str, ...)
   assert(type(str)=='string', 'expecting string')
-  local searchlist = arg
+  local searchlist = ...
   local estring,evar
 
-  function estring(str)
+  function estring(_str)
     local b,e,i
-    b,i = strfind(str, '%$.')
+    b,i = strfind(_str, '%$.')
     if not b then return str end
 
     local R, pos = {}, 1
     repeat
-      b,e = strfind(str, '^%b{}', i)
+      b,e = strfind(_str, '^%b{}', i)
       if b then
-        push(R, strsub(str, pos, b-2))
-        push(R, evar(strsub(str, b+1, e-1)))
+        push(R, strsub(_str, pos, b-2))
+        push(R, evar(strsub(_str, b+1, e-1)))
         i = e+1
         pos = i
       else
-        b,e = strfind(str, '^%b()', i)
+        b,e = strfind(_str, '^%b()', i)
         if b then
-          push(R, strsub(str, pos, b-2))
-          push(R, evar(strsub(str, b+1, e-1)))
+          push(R, strsub(_str, pos, b-2))
+          push(R, evar(strsub(_str, b+1, e-1)))
           i = e+1
           pos = i
-        elseif strfind(str, '^%a', i) then
-          push(R, strsub(str, pos, i-2))
-          push(R, evar(strsub(str, i, i)))
+        elseif strfind(_str, '^%a', i) then
+          push(R, strsub(_str, pos, i-2))
+          push(R, evar(strsub(_str, i, i)))
           i = i+1
           pos = i
-        elseif strfind(str, '^%$', i) then
-          push(R, strsub(str, pos, i))
+        elseif strfind(_str, '^%$', i) then
+          push(R, strsub(_str, pos, i))
           i = i+1
           pos = i
         end
       end
-      b,i = strfind(str, '%$.', i)
+      b,i = strfind(_str, '%$.', i)
     until not b
 
-    push(R, strsub(str, pos))
+    push(R, strsub(_str, pos))
     return concat(R)
   end
 
@@ -68,7 +69,7 @@ function expand(str, ...)
     error('unknown variable: '.. index)
   end
 
-  local function elist(var, v, str, sep)
+  local function elist(var, v, _str, sep)
     local tab = search(v)
     if tab then
       assert(type(tab)=='table', 'expecting table from: '.. var)
@@ -77,7 +78,7 @@ function expand(str, ...)
       push(searchlist, 1, false)
       for _,elem in ipairs(tab) do
         searchlist[1] = elem
-        push(R, estring(str))
+        push(R, estring(_str))
       end
       pop(searchlist, 1)
       pop(searchlist, 1)
@@ -106,15 +107,15 @@ function expand(str, ...)
     if strfind(var, '^[_%a][_%w]*$') then -- ${vn}
       return estring(tostring(search(var)))
     end
-    local b,e,cmd = strfind(var, '^(%a+)%s.')
+    local _,e,cmd = strfind(var, '^(%a+)%s.')
     if cmd == 'foreach' then -- ${foreach vn xxx} or ${foreach vn/sep/xxx}
       local vn,s
-      b,e,vn,s = strfind(var, '^([_%a][_%w]*)([%s%p]).', e)
+      _,e,vn,s = strfind(var, '^([_%a][_%w]*)([%s%p]).', e)
       if vn then
         if strfind(s, '%s') then
           return elist(var, vn, strsub(var, e), '')
         end
-        b = strfind(var, s, e, true)
+        local b = strfind(var, s, e, true)
         if b then
           return elist(var, vn, strsub(var, b+1), strsub(var,e,b-1))
         end
@@ -122,7 +123,7 @@ function expand(str, ...)
       error('syntax error in: '.. var, 2)
     elseif cmd == 'when' then -- $(when vn xxx)
       local vn
-      b,e,vn = strfind(var, '^([_%a][_%w]*)%s.', e)
+      _,e,vn = strfind(var, '^([_%a][_%w]*)%s.', e)
       if vn then
         local t = search(vn)
         if not t then
